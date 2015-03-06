8f18b9f121c7b7264330a44b5612fdbf7cdb46e8
NiklasRolleberg <niklasr93@gmail.com>
2015-03-06 15:40:12 +0100

    new launcher and acivitys

---
+++ app/src/main/java/simtek/gameplanner/android/Activity_launcher.java
@@ -0,0 +1,70 @@
+package simtek.gameplanner.android;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import simtek.gameplanner.R;
+
+public class Activity_launcher extends ActionBarActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        //Find buttons
+        Button startAcrivity01 = (Button)findViewById(R.id.button);
+        Button startAcrivity02 = (Button)findViewById(R.id.button2);
+        Button startAcrivity03 = (Button)findViewById(R.id.button3);
+
+        //add OnClicklistener
+        startAcrivity01.setOnClickListener(this);
+        startAcrivity02.setOnClickListener(this);
+        startAcrivity03.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_launcher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button) {
+            Intent intent = new Intent(this, Activity_01.class);
+            startActivity(intent);
+        }
+        else if (v.getId() == R.id.button2) {
+            Intent intent = new Intent(this, Activity_02.class);
+            startActivity(intent);
+        }
+        else if (v.getId() == R.id.button3) {
+            Intent intent = new Intent(this, Activity_03.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/simtek/gameplanner/model/Model.java
@@ -0,0 +1,10 @@
+package simtek.gameplanner.model;
+
+/**
+ * Created by Niklas on 2015-03-06.
+ */
+public class Model {
+    public Model() {
+        System.out.println("MODEL LEVER!");
+    }
+}
