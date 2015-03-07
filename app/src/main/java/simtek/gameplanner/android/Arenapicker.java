5bc6fa405c1334efa2362973c29ca4aa0319a3f2
KotzDev <lubomirkotzev@gmail.com>
2015-03-07 16:34:18 +0100

    some ugly layout
    usch

--- app/src/main/java/simtek/gameplanner/android/Arenapicker.java
+++ app/src/main/java/simtek/gameplanner/android/Arenapicker.java
@@ -4,15 +4,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import simtek.gameplanner.R;
 
-public class Arenapicker extends ActionBarActivity {
-
+public class Arenapicker extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
+    int defaulArenaID;
+    Spinner vSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.arenapicker_layout);
+        TextView test = (TextView) findViewById(R.id.arenapicker_lbl_pick);
+        defaulArenaID = getIntent().getIntExtra("ID", 1);
+        vSpinner = (Spinner) findViewById(R.id.venueSpinner);
+        //SELECT name FROM arenas, later: SELECT ID FROM arenas WHERE name = "chosen item thingy"
+        String[] items = new String[]{"Super mega stadium 1", "Awesome arena 42"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        vSpinner.setAdapter(adapter);
+        vSpinner.setOnItemSelectedListener(this);
+
+
+
+
     }
 
 
@@ -36,5 +56,15 @@ public class Arenapicker extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/simtek/gameplanner/android/Gameinfo_Activity.java
+++ app/src/main/java/simtek/gameplanner/android/Gameinfo_Activity.java
@@ -1,13 +1,28 @@
 package simtek.gameplanner.android;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import simtek.gameplanner.R;
 
-public class Gameinfo_Activity extends ActionBarActivity {
+public class Gameinfo_Activity extends ActionBarActivity implements View.OnClickListener{
+
+    TextView homeTeam;
+    TextView awayTeam;
+    TextView arena;
+    TextView kickoffTime;
+    TextView referee;
+    TextView umpire;
+    TextView headLinesman;
+    TextView linesman;
+    TextView backJudge;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +32,22 @@ public class Gameinfo_Activity extends ActionBarActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) { //todo pass gameID
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_activity_02, menu);
+        homeTeam = (TextView) findViewById(R.id.gameinfo_homeTeam);
+        awayTeam = (TextView) findViewById(R.id.gameinfo_awayTeam);
+        arena = (TextView) findViewById(R.id.gameinfo_Arena);
+        kickoffTime = (TextView) findViewById(R.id.gameinfo_kickoffTime);
+        referee = (TextView) findViewById(R.id.gameinfo_referee);
+        umpire = (TextView) findViewById(R.id.gameinfo_umpire);
+        headLinesman = (TextView) findViewById(R.id.gameinfo_headLinesman);
+        linesman = (TextView) findViewById(R.id.gameinfo_linesman);
+        backJudge = (TextView) findViewById(R.id.gameinfo_backJudge);
+        setValues(1);
+        arena.setOnClickListener(this);
+
+
         return true;
     }
 
@@ -36,5 +64,26 @@ public class Gameinfo_Activity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.gameinfo_Arena) {
+            Intent intent = new Intent(this, Arenapicker.class);
+            int arenaID = 1337; //todo get from db
+            intent.putExtra("ID",arenaID);
+
+            //Bundle bundle = new Bundle();
+            //bundle.putInt("arenaID",arenaID);
+            //intent.putExtras(bundle);
+            startActivity(intent);
+        }
+
+    }
+    private void setValues(int gameID){ //TODO get from db
+        homeTeam.setText("Home team: " + "STU");
+        referee.setText("R: John Doe");
+        umpire.setText("U: Lubo!!!");
+
     }
 }
