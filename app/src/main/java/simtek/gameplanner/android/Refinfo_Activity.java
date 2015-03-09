614b8f8f67a100831f689cb36b8b4406172f361c
hannahx <milger@kth.se>
2015-03-09 13:11:27 +0100

    titles and thing

--- app/src/main/java/simtek/gameplanner/android/Refinfo_Activity.java
+++ app/src/main/java/simtek/gameplanner/android/Refinfo_Activity.java
@@ -51,7 +51,7 @@ public class Refinfo_Activity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_refinfo_, menu);
-        return true;
+        return false;
     }
 
     @Override
