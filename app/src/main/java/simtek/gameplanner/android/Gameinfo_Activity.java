c8a37dd4e8b780cbf84fdabfd1f1392ae518bb0a
KotzDev <lubomirkotzev@gmail.com>
2015-03-17 16:52:46 +0100

    slim layout for gameinfo
    still not done!

--- app/src/main/java/simtek/gameplanner/android/Gameinfo_Activity.java
+++ app/src/main/java/simtek/gameplanner/android/Gameinfo_Activity.java
@@ -24,24 +24,26 @@ public class Gameinfo_Activity extends ActionBarActivity implements View.OnClick
     TextView awayTeam;
     TextView arena;
     TextView kickoffTime;
-    TextView referee;
-    TextView umpire;
-    TextView headLinesman;
-    TextView linesman;
-    TextView backJudge;
+    //TextView referee;
+    //TextView umpire;
+    //TextView headLinesman;
+    //TextView linesman;
+    //TextView backJudge;
     TextView turnout;
     TextView ticketPrice;
     TextView visitors;
     TextView offPay;
     TextView finalRevenue;
     TextView income;
+    TextView officials;
     int gameID;
+    int refCount;
     Game myGame;
     Arena myArena;
     Model myModel;
-    String[] refTitles;
-    TextView[] refTextViewList;
-    Official[] officials; //good idea, but needs a way to handle nulls
+    //String[] refTitles;
+    //TextView[] refTextViewList;
+    //Official[] officialsArray;
     @Override
     public void onResume() {
         super.onResume();  // Always call the superclass method first
@@ -56,28 +58,29 @@ public class Gameinfo_Activity extends ActionBarActivity implements View.OnClick
         awayTeam = (TextView) findViewById(R.id.gameinfo_awayTeam);
         arena = (TextView) findViewById(R.id.gameinfo_Arena);
         kickoffTime = (TextView) findViewById(R.id.gameinfo_kickoffTime);
-        referee = (TextView) findViewById(R.id.gameinfo_referee);
-        umpire = (TextView) findViewById(R.id.gameinfo_umpire);
-        headLinesman = (TextView) findViewById(R.id.gameinfo_headLinesman);
-        linesman = (TextView) findViewById(R.id.gameinfo_linesman);
-        backJudge = (TextView) findViewById(R.id.gameinfo_backJudge);
+        officials = (TextView) findViewById(R.id.gameinfo_officials);
+        //referee = (TextView) findViewById(R.id.gameinfo_referee);
+        //umpire = (TextView) findViewById(R.id.gameinfo_umpire);
+        //headLinesman = (TextView) findViewById(R.id.gameinfo_headLinesman);
+        //linesman = (TextView) findViewById(R.id.gameinfo_linesman);
+        //backJudge = (TextView) findViewById(R.id.gameinfo_backJudge);
         turnout = (TextView) findViewById(R.id.gameinfo_turnout);
         ticketPrice = (TextView) findViewById(R.id.gameinfo_ticketPrice);
         offPay = (TextView) findViewById(R.id.gameinfo_officialsCost);
         finalRevenue = (TextView) findViewById(R.id.gameinfo_finalRevenue);
         income = (TextView) findViewById(R.id.gameinfo_income);
-        refTextViewList = new TextView[5];
-        refTextViewList[0]= referee;
-        refTextViewList[1]= umpire;
-        refTextViewList[2]= headLinesman;
-        refTextViewList[3]= linesman;
-        refTextViewList[4]= backJudge;
-        refTitles = new String[5];
-        refTitles[0] = " R: ";
-        refTitles[1] = " U: ";
-        refTitles[2] = " HL: ";
-        refTitles[3] = " L: ";
-        refTitles[4] = " BJ: ";
+        //refTextViewList = new TextView[5];
+        //refTextViewList[0]= referee;
+        //refTextViewList[1]= umpire;
+        //refTextViewList[2]= headLinesman;
+        //refTextViewList[3]= linesman;
+        //refTextViewList[4]= backJudge;
+        //refTitles = new String[5];
+        //refTitles[0] = " R: ";
+        //refTitles[1] = " U: ";
+        //refTitles[2] = " HL: ";
+        //refTitles[3] = " L: ";
+        //refTitles[4] = " BJ: ";
 
 
 
@@ -86,17 +89,18 @@ public class Gameinfo_Activity extends ActionBarActivity implements View.OnClick
 
 
         arena.setOnClickListener(this);
-        referee.setOnClickListener(this);
-        umpire.setOnClickListener(this);
-        headLinesman.setOnClickListener(this);
-        linesman.setOnClickListener(this);
-        backJudge.setOnClickListener(this);
+        officials.setOnClickListener(this);
+        //referee.setOnClickListener(this);
+        //umpire.setOnClickListener(this);
+        //headLinesman.setOnClickListener(this);
+        //linesman.setOnClickListener(this);
+        //backJudge.setOnClickListener(this);
 
         myModel = ((CustomApplication) this.getApplication()).getModel();
 
         //get ID
         gameID = getIntent().getIntExtra("ID", 1);
-        officials = new Official[5];
+        //officialsArray = new Official[5];
 
         setValues(gameID);
     }
@@ -128,22 +132,27 @@ public class Gameinfo_Activity extends ActionBarActivity implements View.OnClick
     public void onClick(View v) {
         int clickedID = v.getId();
         ArrayList<Integer> refArray = new ArrayList<>(5);
-        refArray.add(R.id.gameinfo_referee);
-        refArray.add(R.id.gameinfo_umpire);
-        refArray.add(R.id.gameinfo_headLinesman);
-        refArray.add(R.id.gameinfo_linesman);
-        refArray.add(R.id.gameinfo_backJudge);
+        //refArray.add(R.id.gameinfo_referee);
+        //refArray.add(R.id.gameinfo_umpire);
+        //refArray.add(R.id.gameinfo_headLinesman);
+        //refArray.add(R.id.gameinfo_linesman);
+        //refArray.add(R.id.gameinfo_backJudge);
         if (clickedID == R.id.gameinfo_Arena) {
             Intent intent = new Intent(this, Arenapicker.class);
             intent.putExtra("ID", gameID);
             intent.putExtra("arenaID", myGame.getArena().getID());
             startActivity(intent);
         }
-        if (refArray.contains(clickedID)){
+        if (clickedID == R.id.gameinfo_officials){
             Intent intent = new Intent(this, Officialspicker_activity.class);
             intent.putExtra("ID", gameID);
             startActivity(intent);
         }
+        /*if (refArray.contains(clickedID)){
+            Intent intent = new Intent(this, Officialspicker_activity.class);
+            intent.putExtra("ID", gameID);
+            startActivity(intent);
+        }*/
 
     }
     private void setValues(int gameID){
@@ -152,24 +161,27 @@ public class Gameinfo_Activity extends ActionBarActivity implements View.OnClick
             myArena = myGame.getArena();
         }
 
-        arena.setText(myArena.getName());
+        arena.setText("Current venue: " + myArena.getName());
+
         homeTeam.setText("Home team: " + myGame.getHomeTeam().getName());
         awayTeam.setText("Away team: " + myGame.getAwayTeam().getName());
         ticketPrice.setText("Ticket price: " + myGame.getTicketPrice());
         turnout.setText("Turnout [%]: " + myGame.getTurnout());
         visitors.setText("Visitors: " + myGame.getVisitors());
-        referee.setBackgroundResource(R.drawable.red_field);
-        umpire.setBackgroundResource(R.drawable.red_field);
-        headLinesman.setBackgroundResource(R.drawable.red_field);
-        linesman.setBackgroundResource(R.drawable.red_field);
-        backJudge.setBackgroundResource(R.drawable.red_field);
+        //referee.setBackgroundResource(R.drawable.red_field);
+        //umpire.setBackgroundResource(R.drawable.red_field);
+        //headLinesman.setBackgroundResource(R.drawable.red_field);
+        //linesman.setBackgroundResource(R.drawable.red_field);
+        //backJudge.setBackgroundResource(R.drawable.red_field);
         for (int i = 0; i < 5; i++){
             Official temp = myGame.getOfficial(i);
             if (temp != null){
-                officials[i] = temp;
+                //officialsArray[i] = temp;
+                refCount++;
             }
         }
-
+        officials.setText("Officials assigned: " + refCount + "/5");
+        refCount = 0;
 
         String hourString = "" + myGame.getHour();
         String minString = "" + myGame.getMinute();
@@ -190,45 +202,14 @@ public class Gameinfo_Activity extends ActionBarActivity implements View.OnClick
         String date = (year + "-" + month + "-" + day);
 
         kickoffTime.setText(date + ", " + timeString);
-        for (int i = 0; i < 5; i++){
-            //doStuff
-            if (officials[i] != null){
-                refTextViewList[i].setText(refTitles[i] + officials[i].getName());
+        /*for (int i = 0; i < 5; i++){
+            if (officialsArray[i] != null){
+                refTextViewList[i].setText(refTitles[i] + officialsArray[i].getName());
                 refTextViewList[i].setBackgroundResource(R.drawable.tiledesign);
             }
             refTextViewList[i].setPadding(3,0,3,0);
 
-        }
-        /*
-        Official tempOff = myGame.getOfficial(0);
-        if (tempOff != null){
-            referee.setText("R: " + tempOff.getName());
-            referee.setBackgroundResource(R.drawable.tiledesign);
-            referee.setPadding(0,0,0,0);
-        }
-        tempOff = myGame.getOfficial(1);
-        if (tempOff != null){
-            umpire.setText("U: " + tempOff.getName());
-            umpire.setBackgroundResource(R.drawable.tiledesign);
-        }
-        tempOff = myGame.getOfficial(2);
-        if (tempOff != null){
-            headLinesman.setText("HL: " + tempOff.getName());
-            headLinesman.setBackgroundResource(R.drawable.tiledesign);
-        }
-        tempOff = myGame.getOfficial(3);
-        if (tempOff != null){
-            linesman.setText("L: " + tempOff.getName());
-            linesman.setBackgroundResource(R.drawable.tiledesign);
-        }
-        tempOff = myGame.getOfficial(4);
-        if (tempOff != null){
-            backJudge.setText("BJ: " + tempOff.getName());
-            backJudge.setBackgroundResource(R.drawable.tiledesign);
-        }
-        */
-
-
+        }*/
     }
 
     @Override
